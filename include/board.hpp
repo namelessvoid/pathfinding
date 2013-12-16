5c08b41f8c1a2911de958fd1c18483508b2bc38e
namelessvoid <pommesdiefritte@gmx.de>
2013-12-16 22:25:06 +0100

    Now foreward declaring Square in board.hpp.

--- include/board.hpp
+++ include/board.hpp
@@ -1,14 +1,14 @@
 #ifndef BOARD_HPP
 #define BOARD_HPP
 
-#include "square.hpp"
+class Square;
 
 class Board
 {
 	public:
 		Board(int height, int width);
 		~Board();
-		
+
 	private:
 		Square* squares;
 
