947ff0a34c21679b37e7778cabe62f9668ab91e7
namelessvoid <pommesdiefritte@gmx.de>
2013-12-18 21:24:23 +0100

    AlgorithmInterface::getPath() now returns Path instance.

--- include/algorithminterface.hpp
+++ include/algorithminterface.hpp
@@ -1,13 +1,15 @@
 #ifndef ALGORITHM_INTERFACE
 #define ALGORITHM_INTERFACE
 
+#include "path.hpp"
+
 class AlgorithmInterface
 {
 	public:
 		virtual void setStartPoint(int x, int y) = 0;
 		virtual void setEndPont(int x, int y) = 0;
 
-		virtual Node* getPath() = 0;
+		virtual Path* getPath() = 0;
 };
 
 #endif
