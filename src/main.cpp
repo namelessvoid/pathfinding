94de0c4f26c1a5d7819923cd8b6ab0e960a10427
namelessvoid <pommesdiefritte@gmx.de>
2013-12-21 19:27:26 +0100

    Added some hard-coded walls to main().

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <SFML/Graphics/View.hpp>
 
 #include "board.hpp"
+#include "square.hpp"
 #include "boardrenderer.hpp"
 #include "algorithminterface.hpp"
 
@@ -11,6 +12,30 @@
 int main(int arg, char* argv[])
 {
 	pathfinding::Board board(10, 10);
+	board.getSquare(0, 1)->setContent(pathfinding::Square::Content::EC_WALL);
+	board.getSquare(1, 1)->setContent(pathfinding::Square::Content::EC_WALL);
+	board.getSquare(2, 1)->setContent(pathfinding::Square::Content::EC_WALL);
+
+	board.getSquare(3, 1)->setContent(pathfinding::Square::Content::EC_WALL);
+	board.getSquare(3, 2)->setContent(pathfinding::Square::Content::EC_WALL);
+	board.getSquare(3, 3)->setContent(pathfinding::Square::Content::EC_WALL);
+	board.getSquare(3, 4)->setContent(pathfinding::Square::Content::EC_WALL);
+	board.getSquare(3, 5)->setContent(pathfinding::Square::Content::EC_WALL);
+	board.getSquare(3, 6)->setContent(pathfinding::Square::Content::EC_WALL);
+	board.getSquare(3, 7)->setContent(pathfinding::Square::Content::EC_WALL);
+	board.getSquare(3, 8)->setContent(pathfinding::Square::Content::EC_WALL);
+	board.getSquare(3, 9)->setContent(pathfinding::Square::Content::EC_WALL);
+
+	board.getSquare(4, 5)->setContent(pathfinding::Square::Content::EC_WALL);
+	board.getSquare(5, 5)->setContent(pathfinding::Square::Content::EC_WALL);
+	board.getSquare(6, 5)->setContent(pathfinding::Square::Content::EC_WALL);
+	board.getSquare(7, 5)->setContent(pathfinding::Square::Content::EC_WALL);
+
+	board.getSquare(5, 8)->setContent(pathfinding::Square::Content::EC_WALL);
+	board.getSquare(6, 8)->setContent(pathfinding::Square::Content::EC_WALL);
+	board.getSquare(7, 8)->setContent(pathfinding::Square::Content::EC_WALL);
+	board.getSquare(8, 8)->setContent(pathfinding::Square::Content::EC_WALL);
+	board.getSquare(9, 8)->setContent(pathfinding::Square::Content::EC_WALL);
 
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Pathfinding Visualizer", sf::Style::Titlebar | sf::Style::Close);
 	window.setVerticalSyncEnabled(true);
