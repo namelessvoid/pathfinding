0f93ae0ee7d4c05508a95131d23393285d41ce23
namelessvoid <pommesdiefritte@gmx.de>
2013-12-24 14:19:55 +0100

    Fixed broken whitespaces.

--- src/main.cpp
+++ src/main.cpp
@@ -55,34 +55,34 @@ int main(int arg, char* argv[])
 
 	while (window.isOpen())
 	{
-	    // check all the window's events that were triggered since the last iteration of the loop
-	    sf::Event event;
-	    while (window.pollEvent(event))
-	    {
-	        // "close requested" event: we close the window
-	        if (event.type == sf::Event::Closed)
-	            window.close();
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed)
+				window.close();
 
-	        else if (event.type == sf::Event::Resized)
-	        {
-	            // update the view to the new size of the window
-	            sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
-	            window.setView(sf::View(visibleArea));
-	            boardrenderer.resize(event.size);
-	        }
+			else if (event.type == sf::Event::Resized)
+			{
+				// update the view to the new size of the window
+				sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+				window.setView(sf::View(visibleArea));
+				boardrenderer.resize(event.size);
+			}
 
-	        if(boardrenderer.handleEvent(event) == true)
-	        {
-	        	delete path;
-	        	path = algorithm->getPath();
-	        	boardrenderer.setPath(path);
-	        }
-	    }
+			if(boardrenderer.handleEvent(event) == true)
+			{
+				delete path;
+				path = algorithm->getPath();
+				boardrenderer.setPath(path);
+			}
+		}
 
-	    // Rendering
-	    window.clear(sf::Color::Black);
-	    window.draw(boardrenderer);
-	    window.display();
+		// Rendering
+		window.clear(sf::Color::Black);
+		window.draw(boardrenderer);
+		window.display();
 	}
 
 	return 0;
